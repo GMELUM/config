0fe1aba3029362aa2e679db3f72abd145f6fc6d2
GMELUM <gmelum@vk.com>
2024-05-07 04:52:37 +0300

    add key script

---
+++ key.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+USERNAME=${1:-"root"}
+SSH_KEY=${2:-"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPGT2AHuhMZoVmJ0AxqLnG6/sAnzfGNRzDyc5Z8GvXUe master"}
+
+mkdir -p $USERNAME/.ssh
+echo $SSH_KEY >>$USERNAME/.ssh/authorized_keys
+chmod 700 $USERNAME/.ssh
+chmod 600 $USERNAME/.ssh/authorized_keys
+chown -R $USERNAME:$USERNAME $USERNAME/.ssh
