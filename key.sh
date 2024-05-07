cd52e5750314556d9fc4bb6323d22ff5d39b32e5
GMELUM <gmelum@vk.com>
2024-05-07 05:06:34 +0300

    add key script

--- key.sh
+++ key.sh
@@ -8,3 +8,5 @@ echo $SSH_KEY >>$USERNAME/.ssh/authorized_keys
 chmod 700 $USERNAME/.ssh
 chmod 600 $USERNAME/.ssh/authorized_keys
 chown -R $USERNAME:$USERNAME $USERNAME/.ssh
+
+systemctl restart sshd
